12c8a2f7d2d0fa424618374b754b880118f42984
dandridge-cole <dandridgecole@yahoo.com>
2019-10-02 15:01:16 -0400

    stubbing out list provider

---
+++ src/providers/list.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from '@angular/common/http';
+
+import { User } from '../models/user';
+import { Observable } from 'rxjs/Observable';
+
+export class ListProvider {
+    apiUrl = 'https://api.github.com';
+
+    constructor(public httpc: HttpClient){}
+
+    loadList(): Observable<User[]>{return this.httpc.get<User[]>(`${this.apiUrl}/usersusers?since=135`)}
+}
