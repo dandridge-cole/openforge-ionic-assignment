53cf3db02a5f846f23726f8835fa19c7969cab63
dandridge-cole <dandridgecole@yahoo.com>
2019-10-02 14:43:17 -0400

    user model added

---
+++ src/models/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+    login: string;
+    avatar_url: string;
+    name: string;
+    bio: string;
+    company: string;
+    location: string;
+    blog: string;
+    public_repos: number;
+}
