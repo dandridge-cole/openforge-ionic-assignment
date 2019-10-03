ee5b47e1e4ae0d76de35c46f302c3b349de174c3
dandridge-cole <dandridgecole@yahoo.com>
2019-10-03 13:01:56 -0400

    adding search control to search tab template

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'page-search',
   templateUrl: 'search.html'
 })
 export class SearchPage {
+  selectedUser:User;
 
   constructor(public navCtrl: NavController) {
 
