e702b55194cfb9ef083dd87d110a1181bf62f7a8
dandridge-cole <dandridgecole@yahoo.com>
2019-10-04 13:17:04 -0400

    first cypress sample test from beginning of tutorial

---
+++ cypress/integration/sample.spec.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+import Chance from 'chance';
+const chance = new Chance();
+
+describe('OpenForgeProject', ()=>{
+ // randomly generate parameters for each test run, eg:
+ //    const email = chance.email();
+ //     const pass = 'ValidPassword23';s
+
+    beforeEach(() => {
+        cy.visit('http://localhost:8100');
+    })
+
+    it('has a title', ()=> {
+        // Assert List page text
+        cy.contains('GitHub Users');
+        expect(2).to.equal(2)
+    });
+});
