9d60b0d63ec3f5b269b0f7038d22f2aec0b59cbd
dandridge-cole <dandridgecole@yahoo.com>
2019-10-05 10:05:11 -0400

    installed NgRX, starting tutorial

---
+++ src/app/reducers/index.ts
@@ -0,0 +1,10 @@
+      
+      
+class State<T>  { // extends BehaviorSubject implements OnDestroy
+    // static INIT: INIT
+    // ngOnDestroy()
+
+   // metaReducers?: MetaReducer<T, V>[];
+  }
+  
+      
