804acf948c732e26ee524a95ba1bb966715529aa
dandridge-cole <dandridgecole@yahoo.com>
2019-10-17 09:22:38 -0400

    moved doInfinite into provider module. Still need to figure out pagination

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -11,66 +11,33 @@ import { ListProvider } from '../../providers/list';
   templateUrl: 'list.html'
 })
 export class ListPage {
-  list$: Observable<User[]>;
-  users: User[];
-  nextBatch: User[];
-  errorMessage:string;
-  since = 0;
-  maxListId = 46;
-  sinceIncrement = 30;
+
 
   constructor(
-    //private store: Store<{list:User[]}>,
     public navCtrl: NavController 
      ,public listProvider:ListProvider
-    ) 
-    {
+    ) {}
+
+    ngOnInit(){
+      //private store: Store<{list:User[]}>;
       // this.list$ = store.pipe(select('list'));
       this.loadList();
     }
 
-    // loadNext() {
-    //   this.store.dispatch(loadNext());
-    // }
 
     loadList(){
-      this.listProvider.loadList(this.since).subscribe(
-        data => this.users = data,
-        error =>  this.errorMessage = <any>error);
+      this.listProvider.loadList();
     }
 
     doInfinite(infiniteScroll) {
-      this.since = this.users[this.users.length-1].id;
-      // console.log("since: "+this.since);
-      setTimeout(() => {
-        this.listProvider.loadList(this.since)
-           .subscribe(
-            data => this.nextBatch = data,
-            error =>  this.errorMessage = <any>error);
-        console.log('Async operation has ended');
-        // console.log("Next batch in1: "+this.nextBatch);
-        infiniteScroll.complete();
-        // console.log("Next batch in2: "+this.nextBatch);
-      }, 1000);
-      console.log("Next batch out: "+this.nextBatch);
-      //console.log("nextbatch length: "+this.nextBatch.length);
-      //console.log("nextBatch max ID: "+this.nextBatch[nextBatch.length-1].id);
-      this.users.concat(this.nextBatch);
+      this.listProvider.doInfinite(infiniteScroll);
     }
 
     userSelect(user:User) {
       console.log("userSelect was called");
       let login = user.login;
-      // console.log("click nexBatch: "+this.nextBatch);
-      // console.log("user: "+user);
-      // console.log("login: "+user.login);
-      // console.log("list length: "+this.users.length);
-      // console.log("since: "+this.since);
       //var detailedUser:User;
       this.listProvider.loadDetail(user.login).subscribe(data => this.navCtrl.parent.selectedUser = data);
-      //console.log("detailedUser: "+detailedUser);
-      //console.log("detUser pubRepos: "+detailedUser.public_repos);
-      //this.navCtrl.parent.selectedUser=user;
       this.navCtrl.parent.selectedLogin=login;
       this.navCtrl.parent.select(1);
     }
--- src/providers/list.ts
+++ src/providers/list.ts
@@ -1,24 +1,48 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { User } from '../models/user';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ListProvider {
   private apiUrl = 'https://api.github.com';
+  private next:string;
   errorMessage:string;
+  users$: Observable<User[]>;
+  since = 0;
+  maxListId = 46;
+  sinceIncrement = 30;
+
 
   constructor(public httpc: HttpClient){}
 
-  loadList(since): Observable<User[]>{
-    return this.httpc.get<User[]>(`${this.apiUrl}/users?since=${since}`)
-            // .map(this.extractData)
-            // .catch(this.handleError)
-            ;
+  loadList(){
+    //const headers = new HttpHeaders().set("X-CustomHttpHeader", "CUSTOM_VALUE");
+    //this.users$ = this.httpc.get<User[]>(`${this.apiUrl}/users?since=${this.since}`, {headers});
+    this.users$ = this.httpc.get<User[]>(`${this.apiUrl}/users?since=${this.since}`);
+    console.log(this.users$.pipe())
+  }
+
+  parse_link_header(header){
+    if (header.length == 0) {
+      return ;
+    }
+
+    let parts = header.split(',');
+    var links = {};
+    parts.forEach( p => {
+      let section = p.split(';');
+      var url = section[0].replace(/<(.*)>/, '$1').trim();
+      var name = section[1].replace(/rel="(.*)"/, '$1').trim();
+      links[name] = url;
+
+    });
+    return links;
   }
 
   loadDetail(login): Observable<User>{
@@ -50,5 +74,18 @@ export class ListProvider {
     }
     console.error(errMsg);
     return Observable.throw(errMsg);
+  }
+
+  doInfinite(infiniteScroll): Observable<User[]>{
+    //this.since = this.users$[this.users$.length-1].id; // doesn't work with observable, need to implement proper pagination.
+    // console.log("since: "+this.since);
+    setTimeout(() => {
+      this.loadList();
+      console.log('Async operation has ended');
+      // console.log("Next batch in1: "+this.nextBatch);
+      infiniteScroll.complete();
+      // console.log("Next batch in2: "+this.nextBatch);
+    }, 1000);
+    return this.users$;
   }
 }
